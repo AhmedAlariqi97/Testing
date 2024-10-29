a016a9d7c55629e0bc7e16d343e6df94a712246d
unknown <ahmedalariqi97@gmail.com>
2024-10-29 19:38:54 +0300

    edit seeder

--- public/sql/seo_management.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `seo_management` (`id`, `title`, `meta`, `keywords`, `created_at`, `updated_at`) VALUES
-(1, '{\"ar\":\"B2 burger\"}', '{\"ar\":\"b2 burger\"}', '{\"ar\":\"b2 burge,burger\"}', '2023-11-27 17:42:55', '2023-11-27 17:42:55');
