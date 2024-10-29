b87b9e572db429a79bf6af327ceb8b28cba4e28f
unknown <ahmedalariqi97@gmail.com>
2024-10-29 19:42:54 +0300

    edit back

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,7 +25,7 @@ class UserSeeder extends Seeder
         $user->addRole('super_admin');
         $user = User::create([
             'name' => 'testing',
-            'email' => 'b2burger@gmail.com',
+            'email' => 'testing@gmail.com',
             'password' => bcrypt('12345678'),
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
